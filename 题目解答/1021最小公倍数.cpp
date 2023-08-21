848291c9c469607556f5a48ddf96155668f83965
yanyuqi <1900387331@qq.com>
2023-08-21 15:55:53 +0800

    rename

--- 题目解答/1021最小公倍数.cpp
+++ 题目解答/1021最小公倍数.cpp
@@ -2,17 +2,20 @@
 // Created by yanyuqi on 2023/8/18.
 //
 #include <iostream>
+
 using namespace std;
+
 /*
  * 辗转相除法求出最小公约数
  * https://www.runoob.com/cplusplus/cpp-examples-lcm.html
  */
-unsigned  int  gcd(unsigned int a,unsigned  b){
-    return b?gcd(b,a%b):a;
+unsigned int gcd(unsigned int a, unsigned b) {
+    return b ? gcd(b, a % b) : a;
 }
-int main(){
-    unsigned  int a,b;
-    cin>>a>>b;
-    cout<<a*b/gcd(a,b);//乘积除以最小公约数即为最小公倍数
-    return  0;
+
+int main() {
+    unsigned int a, b;
+    cin >> a >> b;
+    cout << a * b / gcd(a, b);//乘积除以最小公约数即为最小公倍数
+    return 0;
 }
