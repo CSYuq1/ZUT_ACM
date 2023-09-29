38191bbb9c2a03b3d737f806bbd0a82eedb66a07
yanyuqi <1900387331@qq.com>
2023-09-29 21:18:08 +0800

    rename

--- 题目解答/公倍数2、3、7整除的整数个数.cpp
+++ 题目解答/公倍数2、3、7整除的整数个数.cpp
@@ -1,10 +1,13 @@
+//
+// Created by yanyu on 2023/9/29.
+//
 #include "iostream"
 
 int main() {
     unsigned int n, low = 0, mid, high = 21;
     unsigned int num[22] = {84, 126, 168, 210, 252, 294, 336, 378, 420, 462, 504, 546, 588, 630, 672, 714, 756, 798,
                             840, 882, 924, 966};
-    //100ÒÔÄÚÓÐ2¸ö·ûºÏµÄÊý×Ö
+    //100ä»¥å†…æœ‰2ä¸ªç¬¦åˆçš„æ•°å­—
     std::ios::sync_with_stdio(false);
     std::cin >> n;
     while (low != high - 1) {
