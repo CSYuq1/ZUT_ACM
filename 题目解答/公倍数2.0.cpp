dd78cb0588e89bb53f12623585077c85df106054
yanyuqi <1900387331@qq.com>
2023-09-30 19:52:24 +0800

    1

---
+++ 题目解答/公倍数2.0.cpp
@@ -0,0 +1,10 @@
+//
+// Created by yanyu on 2023/9/30.
+//
+#include <cstdio>
+int main() {
+    unsigned int n;
+    while (scanf("%d", &n) != EOF)
+        printf("%d\n", (n - 84) / 42);
+    return 0;
+}
