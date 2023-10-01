a9fbf918e11758f058ecd2368b877999403070e9
yanyuqi <1900387331@qq.com>
2023-10-01 11:55:47 +0800

    1

---
+++ 题目解答/逆序输出字符串.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    unsigned int n;
+    cin >> n;
+    char str[n];
+    cin >> str;
+    for (unsigned int i = n; i > 0; i--) {
+        cout << str[i - 1];
+    }
+    ret//
+// Created by yanyu on 2023/10/1.
+//
