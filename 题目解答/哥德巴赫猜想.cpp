d04ec65707d0cd153ce62294d982273088a75b9b
SQL-YUQI <1900387331@qq.com>
2023-11-04 09:57:17 +0800

    1

--- 题目解答/哥德巴赫猜想.cpp
+++ 题目解答/哥德巴赫猜想.cpp
@@ -1,22 +1,21 @@
 //
-// Created by lenovo on 2023/10/28.
+// Created by lenovo on 2023/11/4.
 //
-#include <iostream>
+#include "iostream"
 
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int prime[20000];//代表储存的素数，实际上没有这么多素数
     int c = 0;
     bool isVisit[100001];//每个变量代表这个数是不是素数  false->素数
     for (int i = 2; i <= 100000; ++i)//老规矩，遍历区间
     {
-        if (!isVisit[i]) { //如果这个数未被访问，则是素数
+        if (!isVisit[i])  //如果这个数未被访问，则是素数
             prime[++c] = i;//将素数保存在素数数组里面，计数+1
-            //cout<<prime[c]<<endl;
-        }
+
         //下面for循环及里面的语句才是这个算法的精髓
         for (int j = 1; j <= c && i * prime[j] <= 100001; ++j) {
             isVisit[i * prime[j]] = true;//质数相乘所得到的数字都是奇数
@@ -24,23 +23,12 @@ int main() {
                 break;
         }
     }
-
-    /*
-     *上一段用来求素数，这一段用来得到M,N之间素数和
-     */
-    int m, n, start = 0;
-    cin >> m >> n;
-    for (int i = 1; i < 20000; i++)
-        if (prime[i] >= m) {
-            start = i;
-            break;
-        }
-
-    unsigned long long sum = 0;
-    for (int j = start; j < 20000; j++)
-        if (prime[j] <= n)
-            sum += prime[j];
-        else break;
-    cout << sum;
+    int k;
+    cin >> k;
+    for(int i=0;prime[i]<=k/2;i++)
+    {
+        if(isVisit[k-prime[i]])
+            cout<<k<<'='<<prime[i]<<'+'<<k-prime[i]<<'\n';
+    }
     return 0;
 }
