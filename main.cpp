e059c70c82caf910980c5d3acee5b03e074da040
Yuq1_Lap <1900387331@qq.com>
2023-12-12 14:52:19 +0800

    1

--- main.cpp
+++ main.cpp
@@ -3,24 +3,27 @@
 //
 #include "iostream"
 #include "vector"
+#include "string"
+#include "algorithm"
 
 int main() {
     using namespace std;
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int month[12]={31};
-    month[2]=28;
-    month[4]= month[6]= month[9]= month[11]=30;
     int n;
     cin >> n;
     while (n--) {
         int m;
         cin >> m;
-        vector<string> Students;
-        Students.reserve(m);
-        for (auto &s: Students)
+        vector<string> S(m);
+        for (auto &s: S)
             cin >> s;
-
-
+        string target_name;
+        int target_day;
+        cin >> target_name >> target_day;
+        auto item = find(S.begin(), S.end(), target_name);
+        auto target_index = distance(S.begin(), item);
+        cout << S[(target_index + target_day + 333) % m] << '\n';
+        //S.clear();
     }
 }
